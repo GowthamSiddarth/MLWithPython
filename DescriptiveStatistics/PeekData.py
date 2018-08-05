f8ba6641954cc148ff7e066d25f2b3af78cf38e3
Gowtham Behara <gothsiddu@gmail.com>
2018-08-05 18:04:47 +0530

    Peek data using data frame from pandas

---
+++ DescriptiveStatistics/PeekData.py
@@ -0,0 +1,8 @@
+from pandas import read_csv
+
+filename = "../data/pima-indians-diabetes.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+raw_data = open(filename, 'r')
+data = read_csv(raw_data, names=names)
+print(data.shape)
+print(data.head())
