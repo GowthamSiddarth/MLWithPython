6d8240694c2a538f6dcab98b9ef4e48725853bcb
Gowtham Behara <gothsiddu@gmail.com>
2018-08-05 18:25:13 +0530

    Calculate count per class using groupby

---
+++ DescriptiveStatistics/ClassDistribution.py
@@ -0,0 +1,10 @@
+from pandas import read_csv
+
+
+filename = "../data/pima-indians-diabetes.csv"
+raw_data = open(filename, 'r')
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(raw_data, names=names)
+
+classes = data.groupby('class')
+print(classes.size())
