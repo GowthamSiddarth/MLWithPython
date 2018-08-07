34911f61095b5a617f7f9b1bd7e86822390033ac
Gowtham Behara <gothsiddu@gmail.com>
2018-08-07 23:59:06 +0530

    Calculate cross val score for LassoRegression using k_fold

---
+++ SpotCheckRegressionAlgorithms/LassoRegression.py
@@ -0,0 +1,20 @@
+# Lasso Regression
+from pandas import read_csv
+from numpy.random import randint
+from time import time
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.linear_model import Lasso
+
+filename = "../data/housing.csv"
+names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO',
+         'B', 'LSTAT', 'MEDV']
+data_frame = read_csv(filename, delim_whitespace=True, names=names)
+values = data_frame.values
+x, y = values[:, :13], values[:, 13]
+
+num_of_folds, seed, scoring = 10, randint(int(time())), 'neg_mean_squared_error'
+k_fold = KFold(n_splits=num_of_folds, random_state=seed)
+model = Lasso()
+
+results = cross_val_score(model, x, y, scoring=scoring, cv=k_fold)
+print("Results: " + str(results.mean()))
