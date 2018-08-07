32134dce5000718ea5727f94ab4c2a338479a342
Gowtham Behara <gothsiddu@gmail.com>
2018-08-07 14:44:37 +0530

    Calculate AUC metric of model for KFold cross validation

---
+++ PerformanceMetrics/AreaUnderROC.py
@@ -0,0 +1,20 @@
+# Cross Validation Classification AUC ROC
+from pandas import read_csv
+from numpy.random import randint
+from time import time
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+filename = "../data/pima-indians-diabetes.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data_frame = read_csv(filename, names=names)
+values = data_frame.values
+x, y = values[:, :8], values[:, 8]
+
+num_of_folds, seed, scoring = 10, randint(int(time())), 'roc_auc'
+model = LogisticRegression()
+k_fold = KFold(n_splits=num_of_folds, random_state=seed)
+
+results = cross_val_score(model, x, y, scoring=scoring, cv=k_fold)
+print("AUC : %s, %s" % (results.mean(), results.std()))
