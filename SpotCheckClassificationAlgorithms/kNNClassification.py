bd2ebb77a09b52bfbdfec4ef1f30e6e239c4efcf
Gowtham Behara <gothsiddu@gmail.com>
2018-08-07 20:49:19 +0530

    Calculate cross val score for kNNClassifier with KFold

---
+++ SpotCheckClassificationAlgorithms/kNNClassification.py
@@ -0,0 +1,19 @@
+# kNN Classification
+from pandas import read_csv
+from numpy.random import randint
+from time import time
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+filename = "../data/pima-indians-diabetes.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data_frame = read_csv(filename, names=names)
+values = data_frame.values
+x, y = values[:, :8], values[:, 8]
+
+num_of_folds, seed = 10, randint(int(time()))
+model = KNeighborsClassifier()
+
+k_fold = KFold(n_splits=num_of_folds, random_state=seed)
+results = cross_val_score(model, x, y, cv=k_fold)
+print("Results: " + str(results.mean()))
