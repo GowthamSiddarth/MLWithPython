2f451d253ea15ecdb5d3efd1f63602cf67c4c991
Gowtham Behara <gothsiddu@gmail.com>
2018-08-09 00:10:08 +0530

    Use ExtraTreeClassifier with multiple estimatores

---
+++ ImproveWithEnsemble/ExtraTreesClassification.py
@@ -0,0 +1,20 @@
+# Extra Trees Classification
+from pandas import read_csv
+from numpy.random import randint
+from time import time
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.ensemble import ExtraTreesClassifier
+
+filename = "../data/pima-indians-diabetes.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data_frame = read_csv(filename, names=names)
+values = data_frame.values
+x, y = values[:, :8], values[:, 8]
+
+num_of_folds, seed = 10, randint(int(time()))
+k_fold = KFold(n_splits=num_of_folds, random_state=seed)
+
+num_of_trees = 100
+model = ExtraTreesClassifier(n_estimators=num_of_trees, random_state=seed)
+results = cross_val_score(model, x, y, cv=k_fold)
+print(results.mean())
