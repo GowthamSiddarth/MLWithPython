5f9e88bd2738318ea0068ae36fddb846a6210b9c
Gowtham Behara <gothsiddu@gmail.com>
2018-08-09 07:42:36 +0530

    Save and load mlmodels using pickle

---
+++ SaveLoadMLModels/UsingPicke.py
@@ -0,0 +1,25 @@
+# Save & Load Model with pickle
+from pandas import read_csv
+from numpy.random import randint
+from time import time
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from pickle import dump, load
+
+filename = "../data/pima-indians-diabetes.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data_frame = read_csv(filename, names=names)
+values = data_frame.values
+x, y = values[:, :8], values[:, 8]
+
+test_size, seed = 0.33, randint(int(time()))
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=seed)
+model = LogisticRegression()
+model.fit(X=X_train, y=y_train)
+
+filename = "lr_pima_indians_diabetes.mlmodel"
+dump(model, open(filename, "wb"))
+
+model = load(open(filename, "rb"))
+result = model.score(X_test, y_test)
+print(result)
