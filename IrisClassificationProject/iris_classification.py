c28f4dfbfd383204d4f90eee96f25cc10c3533c6
Gowtham Behara <gothsiddu@gmail.com>
2018-08-25 11:58:47 +0530

    Summarize dataframe by desc, skew, corr, peek, shape and dtypes

--- IrisClassificationProject/iris_classification.py
+++ IrisClassificationProject/iris_classification.py
@@ -1,5 +1,5 @@
 # Load libraries
-from pandas import read_csv
+from pandas import read_csv, set_option
 from matplotlib import pyplot as plt
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
@@ -14,3 +14,27 @@ from sklearn.neighbors import KNeighborsClassifier
 filename = '../data/iris.data.txt'
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 data_frame = read_csv(filename, names=names)
+
+# Summarize Data
+print("Data Types of Attributes in DataFrame")
+print(data_frame.dtypes)
+
+peek_size = 10
+print("Dataset dimensions: " + str(data_frame.shape))
+
+print("Data peek: First %d rows" % peek_size)
+print(data_frame.head(n=peek_size))
+
+print("Dataset summary")
+set_option('display.width', 100)
+set_option('precision', 3)
+print(data_frame.describe())
+
+print("Frequency Distribution")
+print(data_frame.groupby('class').size())
+
+print("Correlation Matrix")
+print(data_frame.corr(method='pearson'))
+
+print("Skew Summary")
+print(data_frame.skew())
